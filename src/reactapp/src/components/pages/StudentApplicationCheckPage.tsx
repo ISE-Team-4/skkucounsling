d4f14465b50e258988a1d01a7ae9737828acb124
bwj2800 <bwj2800@naver.com>
2023-11-27 19:12:02 +0900

    [FA-23] 신청서 다운로드 api 연결

--- src/reactapp/src/components/pages/StudentApplicationCheckPage.tsx
+++ src/reactapp/src/components/pages/StudentApplicationCheckPage.tsx
@@ -1,23 +1,32 @@
 import { Button, HStack, Text, VStack, Stack } from "@chakra-ui/react";
 import { observer } from "mobx-react-lite";
 import Appbar from "../Appbar";
-import { Radio, RadioGroup } from '@chakra-ui/react'
-import { Checkbox, CheckboxGroup } from '@chakra-ui/react'
-import { Table, Thead, Tbody, Tr, Th, Td, TableContainer } from '@chakra-ui/react'
-import { DownloadIcon } from '@chakra-ui/icons'
+import { Radio, RadioGroup } from "@chakra-ui/react";
+import { Checkbox, CheckboxGroup } from "@chakra-ui/react";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+} from "@chakra-ui/react";
+import { DownloadIcon } from "@chakra-ui/icons";
 import { useEffect, useState } from "react";
-import { saveAs } from 'file-saver';
+import { saveAs } from "file-saver";
 import { studentStore } from "../../dataflow/store/studentApply/StudentStore";
 import { useNavigate } from "react-router";
 
 const StudentApplyPage = observer(() => {
-  const day = ['일', '월', '화', '수', '목', '금', '토']
+  const day = ["일", "월", "화", "수", "목", "금", "토"];
 
-  const [type, setType] = useState('personal_1');
+  const [type, setType] = useState("personal_1");
   const [fields, setFields] = useState<(string | number)[]>([]);
   const [file, setFile] = useState("");
+  const [file_content, setFileContent] = useState("");
   const [times, setTimes] = useState<(string | number)[]>([]);
-  const [testTime, setTestTime] = useState('10');
+  const [testTime, setTestTime] = useState("10");
   const [testYear, setTestYear] = useState("");
   const [testMonth, setTestMonth] = useState("");
   const [testDate, setTestDate] = useState("");
@@ -29,7 +38,6 @@ const StudentApplyPage = observer(() => {
   useEffect(() => {
     studentStore.fetchApplication();
   }, []);
-
 
   useEffect(() => {
     if (studentStore.applicationExist === 1) {
@@ -49,26 +57,34 @@ const StudentApplyPage = observer(() => {
       setTimes(timeslots);
       setFile(studentStore.application.application_file);
       setTestTime(studentStore.application.test_timeslot);
-      let testdate = studentStore.application.test_date.split('-');
+      let testdate = studentStore.application.test_date.split("-");
       setTestYear(testdate[0]);
       setTestMonth(testdate[1]);
       setTestDate(testdate[2]);
-    }
-    else if (studentStore.applicationExist === 0) {
+    } else if (studentStore.applicationExist === 0) {
       alert("개인 상담 신청 내역이 없습니다.");
       navigate("/student/home");
     }
   }, [studentStore.applicationExist]);
 
   useEffect(() => {
+    setFileContent(studentStore.applicationContent);
+  }, [studentStore.applicationContent]);
+
+  useEffect(() => {
     getExpectedDate();
   }, [times]);
 
   // 상담 예정일 구하기
-  function isExpectedDay(date: Date) { // expected_date의 요일이 times에 있는지 확인하는 함수
+  function isExpectedDay(date: Date) {
+    // expected_date의 요일이 times에 있는지 확인하는 함수
     const dayIndex = date.getDay();
-    const dayAbbreviation = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'][dayIndex];
-    return times.some(time => typeof time === 'string' && time.includes(dayAbbreviation));
+    const dayAbbreviation = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"][
+      dayIndex
+    ];
+    return times.some(
+      (time) => typeof time === "string" && time.includes(dayAbbreviation)
+    );
   }
   function getExpectedDate() {
     let expected_date = new Date(appliedAt);
@@ -88,20 +104,21 @@ const StudentApplyPage = observer(() => {
   }
 
   // 신청서 양식 다운로드
-  const handleDownload = (path: string, saveName: string) => {
+  const handleDownload = (saveName: string, content: string) => {
     // 파일을 읽어오는 동기 함수
-    const loadFile = (url: string) => {
-      const xhr = new XMLHttpRequest();
-      xhr.open('GET', url, false);
-      xhr.send();
-      return xhr.response;
-    };
+    // const loadFile = (url: string) => {
+    //   const xhr = new XMLHttpRequest();
+    //   xhr.open('GET', url, false);
+    //   xhr.send();
+    //   return xhr.response;
+    // };
 
     // docx 파일 읽기
-    const docxContent = loadFile(path);
+    const docxContent = content;
+    // const docxContent = loadFile(path);
 
     // Blob으로 변환
-    const blob = new Blob([docxContent], { type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' });
+    const blob = new Blob([docxContent], { type: "application/octet-stream" });
 
     // 다운로드
     saveAs(blob, saveName);
@@ -122,34 +139,66 @@ const StudentApplyPage = observer(() => {
 
   const ExpectedDate = observer(() => {
     return (
-      <VStack style={{ alignItems: "flex-start", padding: "2rem", width: "100%" }}>
-        <Text fontSize="xl" fontWeight="600">상담 분야</Text>
-        <Text fontSize="md" fontWeight="400" textAlign={"left"}>
-          신청일: {appliedAt?.getFullYear()}년 {appliedAt?.getMonth() + 1}월 {appliedAt?.getDate()}일 ({day[appliedAt.getDay()]})
+      <VStack
+        style={{ alignItems: "flex-start", padding: "2rem", width: "100%" }}
+      >
+        <Text fontSize="xl" fontWeight="600">
+          상담 분야
         </Text>
         <Text fontSize="md" fontWeight="400" textAlign={"left"}>
-          심리 검사일: {testYear}년 {testMonth}월 {testDate}일 ({day[new Date(Number(testYear), Number(testMonth) - 1, Number(testDate)).getDay()]}) {testTime}시
+          신청일: {appliedAt?.getFullYear()}년 {appliedAt?.getMonth() + 1}월{" "}
+          {appliedAt?.getDate()}일 ({day[appliedAt.getDay()]})
         </Text>
         <Text fontSize="md" fontWeight="400" textAlign={"left"}>
-          상담 예정일: {expectedDate?.getFullYear()}년 {expectedDate?.getMonth() + 1}월 {expectedDate?.getDate()}일 ({day[expectedDate.getDay()]})
+          심리 검사일: {testYear}년 {testMonth}월 {testDate}일 (
+          {
+            day[
+              new Date(
+                Number(testYear),
+                Number(testMonth) - 1,
+                Number(testDate)
+              ).getDay()
+            ]
+          }
+          ) {testTime}시
+        </Text>
+        <Text fontSize="md" fontWeight="400" textAlign={"left"}>
+          상담 예정일: {expectedDate?.getFullYear()}년{" "}
+          {expectedDate?.getMonth() + 1}월 {expectedDate?.getDate()}일 (
+          {day[expectedDate.getDay()]})
         </Text>
         <Text fontSize="sm" fontWeight="400" textAlign={"left"}>
-          (*상담 예정일은 실제와 다를 수 있습니다. 심리 상담 후 상담일이 확정됩니다.)
+          (*상담 예정일은 실제와 다를 수 있습니다. 심리 상담 후 상담일이
+          확정됩니다.)
         </Text>
       </VStack>
     );
   });
 
-
   const CounselingType = observer(() => {
     return (
-      <VStack style={{ alignItems: "flex-start", padding: "2rem", width: "100%" }}>
-        <Text fontSize="xl" fontWeight="600">상담 종류</Text>
-        <RadioGroup id="counseling_type" style={{ margin: "1rem" }} onChange={setType} value={type}>
+      <VStack
+        style={{ alignItems: "flex-start", padding: "2rem", width: "100%" }}
+      >
+        <Text fontSize="xl" fontWeight="600">
+          상담 종류
+        </Text>
+        <RadioGroup
+          id="counseling_type"
+          style={{ margin: "1rem" }}
+          onChange={setType}
+          value={type}
+        >
           <Stack>
-            <Radio colorScheme='green' value='personal_1' isDisabled>검사 해석 상담(검사 후 1회기 해석)</Radio>
-            <Radio colorScheme='green' value='personal_5' isDisabled>5회기 개인 상담</Radio>
-            <Radio colorScheme='green' value='personal_10' isDisabled>10회기 개인 상담</Radio>
+            <Radio colorScheme="green" value="personal_1" isDisabled>
+              검사 해석 상담(검사 후 1회기 해석)
+            </Radio>
+            <Radio colorScheme="green" value="personal_5" isDisabled>
+              5회기 개인 상담
+            </Radio>
+            <Radio colorScheme="green" value="personal_10" isDisabled>
+              10회기 개인 상담
+            </Radio>
           </Stack>
         </RadioGroup>
       </VStack>
@@ -158,34 +207,65 @@ const StudentApplyPage = observer(() => {
 
   const CounselingField = observer(() => {
     return (
-      <VStack style={{ alignItems: "flex-start", padding: "2rem", width: "100%" }}>
-        <Text fontSize="xl" fontWeight="600">상담 분야</Text>
-        <CheckboxGroup colorScheme='green' value={fields}>
-          <Stack spacing={[1, 5]} direction={['column', 'row']} style={{ margin: "1rem" }}>
-            <Checkbox value='대인관계' disabled>대인관계</Checkbox>
-            <Checkbox value='성격 및 적응' disabled>성격 및 적응</Checkbox>
-            <Checkbox value='학업 및 진로' disabled>학업 및 진로</Checkbox>
-            <Checkbox value='심리 및 정서' disabled>심리 및 정서</Checkbox>
-            <Checkbox value='가족 관계' disabled>가족 관계</Checkbox>
-            <Checkbox value='결혼 및 연애' disabled>결혼 및 연애</Checkbox>
-            <Checkbox value='종교 및 가치관' disabled>종교 및 가치관</Checkbox>
+      <VStack
+        style={{ alignItems: "flex-start", padding: "2rem", width: "100%" }}
+      >
+        <Text fontSize="xl" fontWeight="600">
+          상담 분야
+        </Text>
+        <CheckboxGroup colorScheme="green" value={fields}>
+          <Stack
+            spacing={[1, 5]}
+            direction={["column", "row"]}
+            style={{ margin: "1rem" }}
+          >
+            <Checkbox value="대인관계" disabled>
+              대인관계
+            </Checkbox>
+            <Checkbox value="성격 및 적응" disabled>
+              성격 및 적응
+            </Checkbox>
+            <Checkbox value="학업 및 진로" disabled>
+              학업 및 진로
+            </Checkbox>
+            <Checkbox value="심리 및 정서" disabled>
+              심리 및 정서
+            </Checkbox>
+            <Checkbox value="가족 관계" disabled>
+              가족 관계
+            </Checkbox>
+            <Checkbox value="결혼 및 연애" disabled>
+              결혼 및 연애
+            </Checkbox>
+            <Checkbox value="종교 및 가치관" disabled>
+              종교 및 가치관
+            </Checkbox>
           </Stack>
         </CheckboxGroup>
       </VStack>
     );
   });
 
-
   const CounselingTime = observer(() => {
     return (
-      <VStack style={{ alignItems: "flex-start", padding: "2rem", width: "100%" }}>
-        <Text fontSize="xl" fontWeight="600">희망 상담 시간</Text>
-        <CheckboxGroup colorScheme='green' value={times}>
+      <VStack
+        style={{ alignItems: "flex-start", padding: "2rem", width: "100%" }}
+      >
+        <Text fontSize="xl" fontWeight="600">
+          희망 상담 시간
+        </Text>
+        <CheckboxGroup colorScheme="green" value={times}>
           <TableContainer style={{ width: "100%" }}>
-            <Table colorScheme="gray" style={{ width: "60%", margin: "1rem" }} size='sm'>
+            <Table
+              colorScheme="gray"
+              style={{ width: "60%", margin: "1rem" }}
+              size="sm"
+            >
               <Thead>
                 <Tr style={{ backgroundColor: "#D9D9D9" }}>
-                  <Th style={{ width: "15%", textAlign: "center" }}>시간\요일</Th>
+                  <Th style={{ width: "15%", textAlign: "center" }}>
+                    시간\요일
+                  </Th>
                   <Th style={{ textAlign: "center" }}>월</Th>
                   <Th style={{ textAlign: "center" }}>화</Th>
                   <Th style={{ textAlign: "center" }}>수</Th>
@@ -195,13 +275,21 @@ const StudentApplyPage = observer(() => {
               </Thead>
               <Tbody>
                 {[10, 11, 13, 14, 15, 16].map((value, idx1) => {
-                  return <Tr>
-                    <Td style={{ backgroundColor: "#D9D9D9" }}>{value}:00~{value + 1}:00</Td>
-                    {['MON', 'TUE', 'WED', 'THU', "FRI"].map((day, idx2) => {
-                      let val = day + (idx1 + 1).toString();
-                      return <Td style={{ textAlign: "center" }}><Checkbox value={val} disabled /></Td>;
-                    })}
-                  </Tr>
+                  return (
+                    <Tr>
+                      <Td style={{ backgroundColor: "#D9D9D9" }}>
+                        {value}:00~{value + 1}:00
+                      </Td>
+                      {["MON", "TUE", "WED", "THU", "FRI"].map((day, idx2) => {
+                        let val = day + (idx1 + 1).toString();
+                        return (
+                          <Td style={{ textAlign: "center" }}>
+                            <Checkbox value={val} disabled />
+                          </Td>
+                        );
+                      })}
+                    </Tr>
+                  );
                 })}
               </Tbody>
             </Table>
@@ -211,14 +299,22 @@ const StudentApplyPage = observer(() => {
     );
   });
 
-
   const FormDownloader = observer(() => {
     return (
-      <VStack style={{ alignItems: "flex-start", padding: "2rem", width: "100%" }}>
-        <Text fontSize="xl" fontWeight="600">제출한 신청서 다운로드</Text>
+      <VStack
+        style={{ alignItems: "flex-start", padding: "2rem", width: "100%" }}
+      >
+        <Text fontSize="xl" fontWeight="600">
+          제출한 신청서 다운로드
+        </Text>
         <Button
-          onClick={() => handleDownload('../../../../djangoapp/' + file, file)}
-          size='sm' leftIcon={<DownloadIcon />} colorScheme="green" style={{ margin: "1rem" }}>신청서 다운로드
+          onClick={() => studentStore.fetchApplicationFile()}
+          size="sm"
+          leftIcon={<DownloadIcon />}
+          colorScheme="green"
+          style={{ margin: "1rem" }}
+        >
+          신청서 다운로드
         </Button>
       </VStack>
     );
--- src/reactapp/src/dataflow/store/studentApply/StudentStore.ts
+++ src/reactapp/src/dataflow/store/studentApply/StudentStore.ts
@@ -1,10 +1,11 @@
-import { IPreferFields } from './../../interface/counseling';
+import { IPreferFields } from "./../../interface/counseling";
 import { remote } from "../../remote/RemoteSource";
 import { IApplicationForm } from "../../interface/counseling";
-import { makeAutoObservable  } from "mobx";
-import { toJS } from 'mobx';
+import { makeAutoObservable } from "mobx";
+import { toJS } from "mobx";
 class StudentStore {
   application!: IApplicationForm;
+  applicationContent!: string;
   applicationExist: number = -1;
   constructor() {
     makeAutoObservable(this);
@@ -15,27 +16,44 @@ class StudentStore {
       .get("counseling/application-student/")
       .onSuccess((json: any) => {
         console.log("fetch success");
-        const applicationsArray= json.counseling_applications ;
-        
-       if(applicationsArray.length===0){
-          this.applicationExist=0;
-       }
-       else{
-        this.application=applicationsArray[0];
-        for (const a of applicationsArray){
-          if(this.application.applied_at<a.applied_at){
-            this.application=a;
+        const applicationsArray = json.counseling_applications;
+
+        if (applicationsArray.length === 0) {
+          this.applicationExist = 0;
+        } else {
+          this.application = applicationsArray[0];
+          for (const a of applicationsArray) {
+            if (this.application.applied_at < a.applied_at) {
+              this.application = a;
+            }
           }
+          this.application.applied_at =
+            this.application.applied_at.split("T")[0];
+          this.applicationExist = 1;
         }
-        this.application.applied_at = this.application.applied_at.split("T")[0];
-        this.applicationExist = 1;
-       }
       })
       .onFailed((code: number, msg?: string) => {
         console.log("fetch failed");
         this.applicationExist = -1;
       })
       .send();
+  };
+
+  fetchApplicationFile = () => {
+    const downloadLink = document.createElement("a");
+    const protocol = window.location.href.split("://")[0];
+    const currentUrl = window.location.href
+      .split("://")[1]
+      .split("/")[0]
+      .replace("3000", "8000");
+    downloadLink.href =
+      protocol +
+      "://" +
+      currentUrl +
+      `/counseling/application-file?application_id=${this.application.id}`;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
   };
 }
 
